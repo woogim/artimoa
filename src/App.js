cfd434bfe9c55a705f26fde648dd969b773081a9
iksw9999 <iksw9999@gmail.com>
2024-08-30 22:49:07 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Slide1 from './slides/Slide1';
 import Slide2 from './slides/Slide2';
 import Slide3 from './slides/Slide3';
 
-
 const IntegratedPresentation = () => {
   const slides = [Slide1, Slide2, Slide3];
   const [currentSlide, setCurrentSlide] = useState(0);
